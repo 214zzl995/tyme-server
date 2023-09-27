a990a662a634c1101a76e19b0392c6f7c27d5358
leri <214zzl995@gmail.com>
2023-09-27 16:57:01 +0800

    rust server verify

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+
+#[macro_use]
+extern crate lazy_static;
+
+mod ssl_publish;
+mod async_subscribe_v5;
+
 fn main() {
+    env_logger::init();
+    // ssl_publish::publish().unwrap();
+    async_subscribe_v5::subscribe().unwrap();
     println!("Hello, world!");
 }
